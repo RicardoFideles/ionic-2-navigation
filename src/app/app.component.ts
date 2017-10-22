ba411d6d0c7656109995f5811583e40947cf3bfe
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-10-22 21:08:07 -0200

    working with navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild} from '@angular/core';
+import { Platform, Nav} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { ContactPage } from '../pages/contact/contact';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+
+  @ViewChild('myNav') nav : Nav;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -16,6 +19,8 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // this.nav.push(ContactPage)
     });
   }
 }
